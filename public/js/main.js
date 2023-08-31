e99037ad8a40dbf0075d31d25a62b10fa64b3b97
ACM-Anik <anikmojumder5252@gmail.com>
2023-08-31 19:19:45 +0600

    dynamic renderer added

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,20 @@
+// --Template Renderer--
+
+let mTemplateRenderer = (data={}, mDOM) => {
+let mDiv = document.createElement("div");
+mDOM.appendChild(mDiv);
+//set..
+mDiv.innerHtml = `Content`;
+
+//do your code..
 console.log("Everything okay");
+  
+};
+
+mTemplateRenderer({
+   "meta": {},
+   "data": {
+    //add params based on content requirement
+    }
+}, document.body);
+
