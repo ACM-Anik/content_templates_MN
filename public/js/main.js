fc78d8ab26b817de36ef85ca247afea458e2657c
ACM-Anik <anikmojumder5252@gmail.com>
2023-09-01 14:28:53 +0600

    updated the initial js code to correct format

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,31 @@
 // --Template Renderer--
 
-let mTemplateRenderer = (data={}, mDOM) => {
-let mDiv = document.createElement("div");
-mDOM.appendChild(mDiv);
-//set..
-mDiv.innerHtml = `Content`;
+let mTemplateRenderer = (data = {}, mDOM) => {
+    let mMain = document.createElement("main");
+    mDOM.appendChild(mMain);
+    //set..
+    mMain.innerHTML = `
+    <div>
+        <h1 style="">Hello World</h1>
+        <p>Here is all the content we have to insert</p>
+        <p>For example, <span style="color: red;">${data.data.item1.text1.val}</span> is from first parameter.</p>
+    </div>
+`;
 
-//do your code..
-console.log("Everything okay");
-  
+
+    //do your code..
+    console.log("Everything okay: ", data.data.item1.text1);
 };
 
 mTemplateRenderer({
-   "meta": {},
-   "data": {
-    //add params based on content requirement
+    "meta": {},
+    "data": {
+        //add params based on content requirement
+        "item1": {
+            "text1": {
+                "val": "ans"
+            }
+        }
     }
 }, document.body);
 
